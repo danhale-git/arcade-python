0b1e408f9b7dc28219dd40b9a49587640b8beb27
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2019-09-04 21:47:04 +0100

    basic entity and input

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import arcade
+import math
 
 # Constants
 SCREEN_WIDTH = 1000
@@ -10,21 +11,69 @@ class MyGame(arcade.Window):
 
     def __init__(self):
 
-        # Call the parent class and set up the window
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        self.player = PlayerEntity
+        self.W_down = False
+        self.S_down = False
+        self.A_down = False
+        self.D_down = False
 
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
 
     def setup(self):
-        """ Set up the game here. Call this function to restart the game. """
-        pass
+        self.player.xPos = SCREEN_WIDTH * 0.5
+        self.player.yPos = SCREEN_HEIGHT * 0.5
+        self.player.speed = 4
+
+    def on_key_press(self, key, modifiers):
+        self.KeyEvent(key, True)
+    
+    def on_key_release(self, key, modifiers):
+        self.KeyEvent(key, False)
+
+    def update(self, delta_time):
+        if self.W_down: self.player.ModifyY(self.player,  1)
+        if self.S_down: self.player.ModifyY(self.player, -1)
+        if self.A_down: self.player.ModifyX(self.player, -1)
+        if self.D_down: self.player.ModifyX(self.player,  1)
 
     def on_draw(self):
-        """ Render the screen. """
-
         arcade.start_render()
-        # Code to draw the screen goes here
+        self.DrawPlayer()        
 
+    def DrawPlayer(self):
+        
+        backRight = (25, -50)
+        backLeft = (-25, -50)
+        front = (0, 0)
+
+        x = self.player.xPos
+        y = self.player.yPos
+
+        arcade.draw_triangle_filled(x+front[0],     y+front[1],
+                                    x+backLeft[0],  y+backLeft[1],
+                                    x+backRight[0], y+backRight[1],
+
+                                    arcade.color.ALLOY_ORANGE)
+
+    def KeyEvent(self, key, state):
+        if   key == arcade.key.W: self.W_down = state
+        elif key == arcade.key.S: self.S_down = state
+        elif key == arcade.key.A: self.A_down = state
+        elif key == arcade.key.D: self.D_down = state
+        
+class PlayerEntity:
+    
+    def __init__(self):
+        self.xPos
+        self.yPos
+        self.speed
+
+    def ModifyY(self, modifier):
+        self.yPos += modifier * self.speed
+    
+    def ModifyX(self, modifier):
+        self.xPos += modifier * self.speed
 
 def main():
     """ Main method """
